013e16aeb144dd94d4717e7e9824037cd511bb94
CryDeveloper <fordefdevat@gmail.com>
2023-04-29 02:47:41 +0300

    init

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using UnitTesting;
+
+namespace UnitTestProject
+{
+    //не нашла аналоги: all, Contains, DoesNotContain, Matches, DoesNotMatch
+    [TestClass]
+    public class UnitTest1
+    {
+        //Подтврждениt, что полученное соответсвует ожидаемому. 
+        //С помощью длины т.к. массивы можно проверить только с помощью определенного метода.
+        [TestMethod]
+        public void EqualTestMethod()
+        {
+            TimeSpan[] startTimes = new TimeSpan[] { new TimeSpan(9, 0, 0), new TimeSpan(10, 0, 0) };
+            int[] durations = new int[] { 20, 30 };
+            string[] result = Calculations.AvailablePeriods(startTimes, durations, new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0), 30);
+            string[] waitResult = new string[] {"08:00-08:30", "08:30-09:00","09:20-09:50",
+                                                "10:30-11:00","11:00-11:30","11:30-12:00" };
+            Assert.AreEqual(waitResult.Length, result.Length);
+        }
+
+        //Проверка, что если есть занятая консультация в начале рабочего дня, то расписание составляется от ее окончания
+        [TestMethod]
+        public void AreNotEqualTestMethod()
+        {
+            TimeSpan[] startTimes = new TimeSpan[] { new TimeSpan(8, 0, 0), new TimeSpan(10, 0, 0) };
+            int[] durations = new int[] { 20, 30 };
+            string[] result = Calculations.AvailablePeriods(startTimes, durations, new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0), 30);
+            Assert.AreNotEqual(("08:00-08:30"), result[0]);
+        }
+        //Проверка соотвтсвия полученного ожидаемому с помощью специального метода
+        [TestMethod]
+        public void TrueTestMethod()
+        {
+            TimeSpan[] startTimes = new TimeSpan[] { new TimeSpan(9, 0, 0), new TimeSpan(10, 0, 0) };
+            int[] durations = new int[] { 20, 30 };
+            string[] result = Calculations.AvailablePeriods(startTimes, durations, new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0), 30);
+            string[] waitResult = new string[] {"08:00-08:30", "08:30-09:00","09:20-09:50",
+                                                "10:30-11:00","11:00-11:30","11:30-12:00" };
+
+            Assert.IsTrue(result.SequenceEqual(waitResult));
+        }
+
+        //Проверка, что есть занятая консультация в конце рабочего дня и она не внесена в 
+        [TestMethod]
+        public void FalseTestMethod()
+        {
+            TimeSpan[] startTimes = new TimeSpan[] { new TimeSpan(11, 30, 0)};
+            int[] durations = new int[] { 20 };
+            string[] result = Calculations.AvailablePeriods(startTimes, durations, new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0), 30);
+            Assert.IsFalse("11:30-12:00" == result[result.Length-1]);
+        }
+
+        //Проверка, что возвращается массив строк 
+        [TestMethod]
+        public void IsInstanceOfTypeTestMethod()
+        {
+            TimeSpan[] startTimes = new TimeSpan[] { new TimeSpan(9, 0, 0), new TimeSpan(10, 0, 0) };
+            int[] durations = new int[] { 20, 30 };
+            object result = Calculations.AvailablePeriods(startTimes, durations, new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0), 30);
+            Assert.IsInstanceOfType(result, typeof(string[]));
+        }
+
+        //Проверка, что возвращается не просто строка
+        [TestMethod]
+        public void IsNotInstanceOfTypeTestMethod()
+        {
+            TimeSpan[] startTimes = new TimeSpan[] { new TimeSpan(9, 0, 0), new TimeSpan(10, 0, 0) };
+            int[] durations = new int[] { 20, 30 };
+            string[] result = Calculations.AvailablePeriods(startTimes, durations, new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0), 30);
+            Assert.IsNotInstanceOfType(result, typeof(string));
+        }
+
+
+        //Проверка, что что метод не работает, когда время консультаций представлено не по порядку
+        [TestMethod]
+        public void IsNullTestMethod()
+        {
+            TimeSpan[] startTimes = new TimeSpan[] { new TimeSpan(10, 0, 0) , new TimeSpan(8, 0, 0) };
+            int[] durations = new int[] { 60, 30 };
+            string[] result = Calculations.AvailablePeriods(startTimes, durations, new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0), 30);
+            Assert.IsNull(result.Length);
+        }
+
+        //Проверка, что что метод в принципе запускается
+        [TestMethod]
+        public void IsNotNullTestMethod()
+        {
+            TimeSpan[] startTimes = new TimeSpan[] { new TimeSpan(8, 0, 0), new TimeSpan(10, 0, 0) };
+            int[] durations = new int[] { 60, 30 };
+            string[] result = Calculations.AvailablePeriods(startTimes, durations, new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0), 30);
+            Assert.IsNotNull(result);
+        }
+
+        //Честно, не знаю что он может проверять в данной ситуации
+        [TestMethod]
+        public void SameTestTestMethod()
+        {
+            TimeSpan[] startTimes = new TimeSpan[] { new TimeSpan(8, 0, 0), new TimeSpan(10, 0, 0) };
+            int[] durations = new int[] { 60, 30 };
+            string[] result = Calculations.AvailablePeriods(startTimes, durations, new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0), 30);
+            Assert.AreSame(result, Calculations.AvailablePeriods(startTimes, durations, new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0), 30));
+        }
+
+        //Честно, не знаю что он может проверять в данной ситуации
+        [TestMethod]
+        public void NotSameTestTestMethod()
+        {
+            TimeSpan[] startTimes = new TimeSpan[] { new TimeSpan(9, 0, 0), new TimeSpan(10, 0, 0) };
+            int[] durations = new int[] { 20, 30 };
+            string[] result = Calculations.AvailablePeriods(startTimes, durations, new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0), 30);
+            Assert.AreNotSame(result, Calculations.AvailablePeriods(startTimes, durations, new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0), 30));
+        }
+
+        //Проверка, что вызывается исключение индексации при определенных условиях
+        [TestMethod]
+        public void ThrowsExceptionTestTestMethod()
+        {
+            TimeSpan[] startTimes = new TimeSpan[] { new TimeSpan(8, 0, 0), new TimeSpan(10, 0, 0) };
+            int[] durations = new int[] { 60, 30 };
+            //string[] result = Calculations.AvailablePeriods(startTimes, durations, new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0), 30);
+            Assert.ThrowsException<IndexOutOfRangeException>(() => Calculations.AvailablePeriods(startTimes, durations, new TimeSpan(8, 0, 0), new TimeSpan(12, 0, 0), 30));
+        }
+    }
+}
---
+++ UnitTesting/Calculations.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace UnitTesting
+{
+    public class Calculations
+    {
+        /// <summary>
+        /// Получение списка свободного времени для консультаций
+        /// </summary>
+        /// <param name="startTimes">Время начала занятых консультаций</param>
+        /// <param name="durations">Время занятых консцультаций</param>
+        /// <param name="beginWorkingTime">Начала рабочего дня</param>
+        /// <param name="endWorkingTime">Конец рабочего дня</param>
+        /// <param name="consultationTime">Стандартное время консультаций</param>
+        /// <returns>Массив строк с временем свободных консультаций</returns>
+        public static string[] AvailablePeriods(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime, TimeSpan endWorkingTime, int consultationTime)
+        {
+            TimeSpan[] listIntervalWorkingTime = new TimeSpan[] { beginWorkingTime };
+            int countBusyTime = 0;
+            if (startTimes[0] == beginWorkingTime)
+            {
+                listIntervalWorkingTime[0] = startTimes[0] + TimeSpan.FromMinutes(durations[0]);
+                countBusyTime++;
+            }
+            for (int i = 0; listIntervalWorkingTime[i] < endWorkingTime; i++)
+            {
+                Array.Resize(ref listIntervalWorkingTime, i + 2);
+                listIntervalWorkingTime[i + 1] = listIntervalWorkingTime[i] + TimeSpan.FromMinutes(30);
+                if (listIntervalWorkingTime[i + 1] >= endWorkingTime)
+                {
+                    Array.Resize(ref listIntervalWorkingTime, listIntervalWorkingTime.Length - 1);
+                    break;
+                }
+                if(countBusyTime < startTimes.Length)
+                {
+                    while (listIntervalWorkingTime[i] <= startTimes[countBusyTime] && startTimes[countBusyTime] <= listIntervalWorkingTime[i + 1])
+                    {
+                        listIntervalWorkingTime[i + 1] = startTimes[countBusyTime] + TimeSpan.FromMinutes(durations[countBusyTime]);
+                        countBusyTime++;
+                        if (countBusyTime >= startTimes.Length)
+                        {
+                            break;
+                        }
+                    }
+                    while (startTimes[countBusyTime] - listIntervalWorkingTime[i + 1] < TimeSpan.FromMinutes(consultationTime) && startTimes[countBusyTime] - listIntervalWorkingTime[i + 1] != TimeSpan.FromMinutes(0))
+                    {
+                        listIntervalWorkingTime[i + 1] = startTimes[countBusyTime] + TimeSpan.FromMinutes(durations[countBusyTime]);
+                        countBusyTime++;
+                        if (countBusyTime >= startTimes.Length)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            
+            string[] availablePeriods = new string[listIntervalWorkingTime.Length];
+            for (int i = 0; i < availablePeriods.Length; i++)
+            {
+                availablePeriods[i] = listIntervalWorkingTime[i].ToString("hh\\:mm") + "-";
+                availablePeriods[i] += (listIntervalWorkingTime[i] + TimeSpan.FromMinutes(30)).ToString("hh\\:mm");
+            }
+            return availablePeriods;
+        }
+    }
+}
